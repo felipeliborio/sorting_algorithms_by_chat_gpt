37a45b1ea7ba65a4f008bf431d605cdd0b9c3fb1
Felipe Libório <felipeliborio@outlook.com>
2023-03-20 07:49:20 -0300

    updating random generator for 32 bit positive integers only and generating new instances

--- implementations/number_generator/src/main.rs
+++ implementations/number_generator/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, Write};
 use std::env; 
 
 fn main() {
-    let input: Vec<u32> = env::args()
+    let input: Vec<i32> = env::args()
         .skip(1)
         .map(|x| x.parse().expect("Not a number!"))
         .collect();
@@ -16,11 +16,11 @@ fn main() {
     io::stdout().flush().unwrap();
 }
 
-fn generate_integers(count: u32) -> Vec<u32> {
+fn generate_integers(count: i32) -> Vec<i32> {
     let mut rng = rand::thread_rng();
-    let mut integers = Vec::new();
+    let mut integers: Vec<i32> = Vec::new();
     for _i in 0..count {
-        integers.push(rng.gen());
+        integers.push(rng.gen_range(0..2000000000));
     }
     integers
 }
