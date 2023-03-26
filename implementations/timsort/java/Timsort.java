bcf8908ee7418f273a04495c729a45808f43fbf9
= <Felipe Librio>
2023-03-26 10:51:49 -0300

    adding correctness check

--- implementations/timsort/java/Timsort.java
+++ implementations/timsort/java/Timsort.java
@@ -34,7 +34,15 @@ public class Timsort {
         outputWriter.print(output);
         outputWriter.close();
 
-        System.out.println("java elapsed seconds "+elapsed/1000.0);
+        boolean isCorrect = true;
+        for (int i = 0; i < input.length - 1; ++i) {
+            if (input[i] > input[i + 1]) {
+                isCorrect = false;
+                break;
+            }
+        }
+
+        System.out.println("java elapsed seconds "+elapsed/1000.0+" | correct: "+isCorrect);
     }
 
     private static final int MIN_MERGE = 32;
